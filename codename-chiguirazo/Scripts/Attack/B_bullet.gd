f0c804cc5131deae6e63121797216338f2f17ccc
Marco <lordneko52@gmail.com>
2020-06-07 00:28:05 -0430

    Ahora la bala es un area

--- codename-chiguirazo/Scripts/Attack/B_bullet.gd
+++ codename-chiguirazo/Scripts/Attack/B_bullet.gd
@@ -1,11 +1,12 @@
-extends RigidBody2D
+extends Area2D
 
-var p_speed :float = 400.0  #Velocidad del proyetil
-var damage : float			#Daño del proyectil
+var p_speed: int = 400  #Velocidad del proyetil
+var damage 			#Daño del proyectil
 
 
-func _ready() -> void:
-	apply_impulse(Vector2(), Vector2(p_speed, 0).rotated(rotation))
-	yield(get_tree().create_timer(5),"timeout")
+func _process(delta: float) -> void:
+		var v_direction: Vector2 = Vector2(1,0).rotated(rotation)  #Calcula la direccion
+		global_position += v_direction * delta * p_speed
+
+func _on_Bullet_body_entered(body):
 	queue_free()
-
